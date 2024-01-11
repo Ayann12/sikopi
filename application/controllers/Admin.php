7587dedaedfe65adb97eb246e0995a97305b6b89
Ayann12 <tantowi818@gmail.com>
2024-01-11 19:26:49 +0700

    menu admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller
+class Admin extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('home');
+        $this->load->view('admin');
     }
 }
