5eece626118e6e676e9d0290552fff072bc35069
Ayann12 <tantowi818@gmail.com>
2024-01-22 22:26:23 +0700

    masih setting2 tampilan admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -5,6 +5,11 @@ class Admin extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = "Halaman Admin";
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/navbar');
+        $this->load->view('templates/sidebar');
         $this->load->view('admin/index');
+        $this->load->view('templates/footer');
     }
 }
--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -5,6 +5,11 @@ class Home extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = "Halaman Admin";
+        $this->load->view('templates/header');
+        $this->load->view('templates/navbar');
+        $this->load->view('templates/sidebar');
         $this->load->view('home/index');
+        $this->load->view('templates/footer');
     }
 }
